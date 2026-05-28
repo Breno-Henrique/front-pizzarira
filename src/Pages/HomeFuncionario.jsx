a36b1f7dbeeffd2264eb67c6220d72080e211272
Breno Henrique Simiao <rm98155@estudante.fieb.edu.br>
2026-05-28 12:07:24 -0300

    menu home e funcionario em produção

---
+++ src/Pages/HomeFuncionario.jsx
@@ -0,0 +1,13 @@
+import MeuFuncionario from "../MenuFuncionario/MeuFuncionario"
+
+const HomeFuncionario = () => {
+return(
+    <div className="Container">
+
+    <MeuFuncionario/>
+    </div>
+)
+
+}
+
+export default HomeFuncionario
